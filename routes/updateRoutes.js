7b5c908a8748e75a8a6dd19a9ed045bbcccb39fd
geon <yy4338@gmail.com>
2024-10-08 15:44:48 +0900

    pull

--- routes/updateRoutes.js
+++ routes/updateRoutes.js
@@ -8,6 +8,6 @@ router.patch('/update_completed_task', updateCompletedTask);
 //patch는 변경 사항에 대한 부분만 업데이트 한다.
 //put은 전체를 업데이트 한다.
 
-router.put('/update_task', updateTask);
+router.pull('/update_task', updateTask);
 
 module.exports = router;
